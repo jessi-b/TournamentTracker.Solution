874cd5067898c9ab45033002b16ca2ee0ee95bbc
jb <jessb.epicodus@gmail.com>
2022-05-06 13:59:12 -0700

    establish initial models

--- Tournament.Solution/TournamentTracker/Models/TournamentTrackerContext.cs
+++ Tournament.Solution/TournamentTracker/Models/TournamentTrackerContext.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace TournamentTracker.Models {
   public class TournamentTrackerContext : DbContext {
-    public DbSet<Competitor> Competitors { get; set; }
-    public DbSet<Tournament> Tournaments { get; set; }
-    public DbSet<Team> Teams { get; set; }
+    public DbSet<Competitor> Competitors {get; set;}
+    public DbSet<Tournament> Tournaments {get; set;}
+    public DbSet<Bracket> Brackets {get; set;}
+    public DbSet<Round> Rounds {get; set;}
+    public DbSet<Match> Matches {get; set;}
+    
     public TournamentTrackerContext(DbContextOptions options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
       optionsBuilder.UseLazyLoadingProxies();
